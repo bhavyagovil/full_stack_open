fbe2ca62c6d8bb7b3227d0ca251b7c66a3a266d5
Bhavya Govil <bhavya0836govil@gmail.com>
2024-06-17 13:06:56 +0100

    exercise 1.10

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,34 +6,25 @@ const Counter = ({text, value}) => {
   )
 }
 
+const StatisticLine = ({text, value}) => {
+  return(
+    <p>{text} {value}</p>
+  )
+}
+
 const Statistics = ({good, neutral, bad, received}) => {
   if (received === 1){return(
     <div>
-      <Counter text='good' value={good}/>
-      <Counter text='neutral' value={neutral}/>
-      <Counter text='bad' value={bad}/>
-      <p>all <Total good={good} neutral={neutral} bad={bad}/></p>
-      <p>average <Average good={good} neutral={neutral} bad={bad}/></p>
-      <p>positive <Positive good={good} neutral={neutral} bad={bad} /></p>
+      <StatisticLine text='good' value={good}/>
+      <StatisticLine text='neutral' value={neutral}/>
+      <StatisticLine text='bad' value={bad}/>
+      <StatisticLine text='all' value={good + neutral + bad}/>
+      <StatisticLine text='average' value={((good * 1) + (neutral * 0) + (bad * -1)) / (good+bad+neutral)}/>
+      <StatisticLine text='positive' value={`${(good/(good+bad+neutral))*100}%`}/>
     </div>
   )} else {return('No feedback given')}
 }
 
-const Total = ({good, neutral, bad}) => {
-  return(good + neutral + bad)
-}
-
-const Average = ({ good, neutral, bad }) => {
-  const total = good + neutral + bad
-  if (total === 0) return 0
-  return ((good * 1) + (neutral * 0) + (bad * -1)) / total
-}
-
-const Positive = ({good, neutral, bad}) => {
-  const total = good + bad + neutral
-  if (total === 0) return 0
-  return(good/total)*100 
-}
 
 const Button = ({handleClick, text}) => {
   return(
