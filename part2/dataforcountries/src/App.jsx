44fdbcfc1784e22633dd5f0edd40e1002b24fdc6
Bhavya Govil <bhavya0836govil@gmail.com>
2024-07-03 11:48:21 +0100

    ex 2.19 w/o buttons

--- part2/dataforcountries/src/App.jsx
+++ part2/dataforcountries/src/App.jsx
@@ -1,61 +1,59 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const Details = ({country}) => {
-  const flagUrl = country.flags.png
-  return(
-    <div>
-  <h1>{country.name.common}</h1>
-  <p>Capital: {country.capital}</p>
-  <p>Area: {country.area}</p>
-  <p><b>Languages:</b></p>
-  <ul> 
-  {(Object.values(country.languages)).map((lang, index) => <li key={index}>{lang}</li>)}
-  </ul>
-  
-  <img src={flagUrl}/>
-  </div>
-  )
+const Details = ({countryToDisplay, showBoolean, buttonClick}) => {
+  const flagUrl = countryToDisplay.flags.png
+  if (showBoolean) {
+    return(
+      <div>
+    <h1>{countryToDisplay.name.common}</h1>
+    <p>Capital: {countryToDisplay.capital}</p>
+    <p>Area: {countryToDisplay.area}</p>
+    <p><b>Languages:</b></p>
+    <ul> 
+    {(Object.values(countryToDisplay.languages)).map((lang, index) => <li key={index}>{lang}</li>)}
+    </ul>
+    <img src={flagUrl}/>
+    </div>
+    )
+  } else {
+    return(
+      <div>
+    <p key={countryToShow.name.common}>{countryToShow.name.common} </p>
+    <button onClick={buttonClick}>show</button>
+    </div>)
+  }
 }
 
-const Display = ({displayCountries}) => {
+const Display = ({displayCountries, buttonClick}) => {
   if(displayCountries.length > 10) {
     return (
       'Too many matches, specify another filter'
     )
   } else if(displayCountries.length === 1) {
-    const detailsOfCountry = displayCountries[0];
+    const country = displayCountries[0]
     return (
       <div>
-        <Details country={detailsOfCountry}/>
+        <Details countryToDisplay={country} showBoolean={'true'} buttonClick={buttonClick}/>
       </div>
     )
-  } else {
+  } else { 
     return (
-      displayCountries.map(country => <p key={country.name.common}>{country.name.common}</p>)
+      displayCountries.map(country => <Details countryToDisplay={country} showBoolean={'false'} buttonClick={buttonClick} />)
     )
   }
 }
 
 const App = () => {
   const [value, setValue] = useState('')
-  const [countries, setCountries] = useState([]) //these are the countries that will be displayed on the screen
-
+  const [countries, setCountries] = useState([]) 
+  const [show, setShow] = useState(false)
   useEffect(() => {
     axios.get('https://studies.cs.helsinki.fi/restcountries/api/all')
   .then(response => {
     setCountries(response.data)
   })
-})
-
-/* const displayTen = countriesToShow => {
-  if (countriesToShow.length >= 10){
-    return(countriesToShow.map(country => country.name.common))
-  } else {
-      'Too many matches, specify another filter'
-    
-  }
-} */
+},[])
 
 const countriesToShow = value === '' 
 ? countries 
@@ -64,12 +62,20 @@ const countriesToShow = value === ''
   const handleChange = (event) => {
     setValue(event.target.value)
   }
+
+  const handleButtonClick = () => {
+    setShow(!show)
+  }
  
   return (
     <div>
-       find countries<input value={value} onChange={handleChange} />
-       <div> <Display displayCountries={countriesToShow}/>
+       find countries <input value={value} onChange={handleChange} />
+       <div> <Display displayCountries={countriesToShow} buttonClick={handleButtonClick} boolean={show}/>
+
+       {/* the value is always truye when passed from here!!! */}
+
        </div>
+       
     </div>
   ) 
 }
