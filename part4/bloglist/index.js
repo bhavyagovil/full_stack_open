a99546e7c280c842a7d023a1ad06cbb33b665d78
Bhavya Govil <bhavya0836govil@gmail.com>
2024-07-30 16:58:45 +0100

    4.1

---
+++ part4/bloglist/index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+
+const blogSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
+  },
+  {versionKey: false})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+app.use(cors())
+app.use(express.json())
+
+const mongoUrl = "mongodb+srv://bhavya0836govil:Iamnotmad08@cluster0.izsjxoz.mongodb.net/bloglist?retryWrites=true&w=majority&appName=Cluster0"
+mongoose.connect(mongoUrl)
+
+
+app.get('/api/blogs', (request, response) => {
+    Blog
+      .find({})
+      .then(blogs => {
+        response.json(blogs)
+      })
+  })
+  
+  app.post('/api/blogs', (request, response) => {
+    const blog = new Blog(request.body)
+  
+    blog
+      .save()
+      .then(result => {
+        response.status(201).json(result)
+      })
+  })
+  
+  const PORT = 3003
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
